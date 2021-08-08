e85d4362b12aff50f8ae26a4ab3a065e2a56a9be
Konstantin Boryagin <nokss89@gmail.com>
2021-08-08 14:53:36 +0300

    refactoring

--- src/main/java/ru/otus/Sorting.java
+++ src/main/java/ru/otus/Sorting.java
@@ -1,10 +1,8 @@
 package ru.otus;
 
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class Sorting {
-
 
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
@@ -12,65 +10,103 @@ public class Sorting {
         ArrayList<Integer> list3 = new ArrayList<>();
         Sorting sort = new Sorting();
 
+        /** Заполняем значениями листы */
         sort.fillTheList(list);
         sort.fillTheList(list2);
         sort.fillTheList(list3);
 
-        Date start = new Date();
-        sort.selectionSorting(list);
-        Date end = new Date();
-        System.out.println("Selection Sort - " + (end.getTime() - start.getTime()) + " ms");
+        sort.countTheTimeSelectionSorting(list);
+        sort.countTheTimeBubbleSorting(list2);
+        sort.countTheTimeQuickSorting(list3);
 
-        long start2 = System.currentTimeMillis();
-        System.out.println(start2);
-        sort.bubbleSorting(list2);
-        long end2 = System.currentTimeMillis();
-        System.out.println(end2);
-        System.out.println("Bubble Sort - " + (end2 - start2)  + " ms");
-
-
-        long start3 = System.currentTimeMillis();
-        System.out.println(start3);
-        Collections.sort(list3);
-        long end3 = System.currentTimeMillis();
-        System.out.println(end3);
-        System.out.println("Quick Sort - " + (end3 - start3)  + " ms");
+//        System.out.println(list);
+//        System.out.println(list2);
+//        System.out.println(list3);
     }
 
+    /**
+     * Заполняет полученный List случайными значениями от 0 до 90
+     * @param list - List чисел
+     * @return - отсортированный List чисел
+     */
     public List<Integer> fillTheList(List<Integer> list) {
         for (int i = 0; i < 1_000_000; i++) {
-            list.add((int) (Math.random() * 40));
+            list.add((int) (Math.random() * 90));
         }
         return list;
     }
 
+    /**
+     * Метод возвращающий List чисел отсортированных с помощью сортировки "выбором"
+     * @param list - List чисел
+     * @return - отсортированный List чисел
+     */
     public List<Integer> selectionSorting(List<Integer> list) {
         for (int i = 0; i < list.size(); i++) {
             for (int j = i + 1; j < list.size(); j++) {
-                int min = list.get(i);
-                int current = list.get(j);
-                if (min > current) {
-                    list.set(i, current);
-                    list.set(j, min);
+
+                if (list.get(i) > list.get(j)) {
+                    swap(list, i, j);
                 }
             }
         }
         return list;
     }
 
+    /**
+     * Метод возвращающий List чисел отсортированных с помощью "пузырьковой" сортировки
+     * @param list - List чисел
+     * @return - отсортированный List чисел
+     */
     public List<Integer> bubbleSorting(List<Integer> list) {
         for (int i = list.size() - 1; i > 0 ; i--) {
             for (int j = 0; j < i; j++) {
-                int left = list.get(j);
-                int right = list.get(j+1);
 
-                if (left > right) {
-                    int temp = left;
-                    list.set(j+1, left);
-                    list.set(j, right);
+                if (list.get(j) > list.get(j+1)) {
+                 swap(list, j, j+1);
                 }
             }
         }
         return list;
+    }
+
+    /**
+     * Метод меняет местами числа в передаваемом листе
+     * @param list - List чисел
+     * @param indexOfLeftNumber - индекс первого элемента
+     * @param indexOfRightNumber - индекс второго элемента
+     */
+    private void swap(List<Integer> list, int indexOfLeftNumber, int indexOfRightNumber) {
+        int leftNumer = list.get(indexOfLeftNumber);
+        int rightNumer = list.get(indexOfRightNumber);
+        list.set(indexOfLeftNumber, rightNumer);
+        list.set(indexOfRightNumber, leftNumer);
+    }
+
+    private void countTheTimeSelectionSorting(List<Integer> list) {
+        Date start = new Date();
+        selectionSorting(list);
+        Date end = new Date();
+        long timeInSec = (end.getTime() - start.getTime()) / 1000;
+        long timeInMin = timeInSec / 60;
+        System.out.println("Selection Sort - " + timeInSec + " sec or " + timeInMin + " min");
+    }
+
+    private void countTheTimeBubbleSorting(List<Integer> list) {
+        Date start = new Date();
+        bubbleSorting(list);
+        Date end = new Date();
+        long timeInSec = (end.getTime() - start.getTime()) / 1000;
+        long timeInMin = timeInSec / 60;
+        System.out.println("Bubble Sort - " + timeInSec + " sec or " + timeInMin + " min");
+    }
+
+    private void countTheTimeQuickSorting(List<Integer> list) {
+        Date start = new Date();
+        Collections.sort(list);
+        Date end = new Date();
+        long timeInSec = (end.getTime() - start.getTime()) / 1000;
+        long timeInMin = timeInSec / 60;
+        System.out.println("Quick Sort - " + timeInSec + " sec or " + timeInMin + " min");
     }
 }
